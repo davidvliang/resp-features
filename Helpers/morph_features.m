5954fb30db2e6c46d34b695d58f7521507ea504a
David Liang <34115092+davidvliang@users.noreply.github.com>
2021-04-14 11:09:11 -1000

    clean morph features code

---
+++ Helpers/morph_features.m
@@ -0,0 +1,109 @@
+function [morph_matrix] = morph_features(time, input_sig, peak_vals)
+%MORPH_FEATURES Summary of this function goes here
+%   Detailed explanation goes here
+
+figure(2);
+% pause;
+
+% initialize feature vectors
+morph_matrix = zeros(size(peak_vals,1)-1, 100); % 100 feats/segment
+
+% trough
+Ht =  0; % height of the wave trough
+Ht_p = 0; % p% of height H_t from minimum point
+It_p = 0; % intercept of the waveform at height Ht_p
+
+% crest
+Hc =  0; % height of the wave crest
+Hc_p = 0; % p% of height H_t from maximum point
+Ic_p = 0; % intercept of the waveform at height Hc_p
+
+
+f_idx = 1;
+for i = 1:1:size(peak_vals,1)-1 % for each segment (later consider overlappin segments)
+    
+    % Figure label
+    clf;
+    sgtitle(['Source 1: ', 'Segment ', num2str(f_idx), ' of ', num2str(size(morph_matrix,1))]);
+    
+    % TROUGH
+    xt = time(peak_vals(i,2):peak_vals(i+1,2),1);
+    yt = input_sig(peak_vals(i,2):peak_vals(i+1,2),1);    
+    Ht = min(peak_vals(i,1), peak_vals(i+1,1)) - peak_vals(i,3); %modified to accomodate for uneven values
+   
+    % CREST
+    xc = time(peak_vals(i,4):peak_vals(i+1,4),1);
+    yc = input_sig(peak_vals(i,4):peak_vals(i+1,4),1);    
+    Hc = peak_vals(i+1,1) - max(peak_vals(i,3), peak_vals(i+1,3)); %modified to accomodate for uneven values
+   
+    p_idx = 1;
+    for p = .02:.02:1
+        
+        % define trough height percentage (Ht_p)
+        Ht_p = Ht * p + peak_vals(i,3);
+        if p == 1   % just to avoid sigfig issue
+            Ht_p = min(peak_vals(i,1), peak_vals(i+1,1)); 
+        end
+        
+        % define crest height percentage (Hc_p)
+        Hc_p = peak_vals(i+1,1) - Hc * p;
+        if p == 1   % just to avoid sigfig issue
+            Hc_p = max(peak_vals(i,3), peak_vals(i+1,3)); 
+        end
+        
+        % find trough intersections
+        [xint_t, ~] = intersections(xt, yt, xt, Ht_p*ones(size(xt)));
+        xint_t_greater = min(xint_t(xint_t>time(peak_vals(i,4))));
+        xint_t_lesser = max(xint_t(xint_t<time(peak_vals(i,4))));
+        
+        % find crest intersections
+        [xint_c, ~] = intersections(xc, yc, xc, Hc_p*ones(size(xc)));
+        xint_c_greater = min(xint_c(xint_c>time(peak_vals(i+1,2))));
+        xint_c_lesser = max(xint_c(xint_c<time(peak_vals(i+1,2))));
+
+        
+        % Compute widths
+        It_p = xint_t_greater - xint_t_lesser;
+        Ic_p = xint_c_greater - xint_c_lesser;
+
+
+        % visualize Ht_p
+        figure(2);
+        subplot(1,2,1);
+        title('trough');
+        hold on;
+        plot(xt, yt, 'Color', 'black');
+        plot(xt, ones(size(xt))*Ht_p, 'Color', 'm'); 
+        plot(time(peak_vals(i,4)), input_sig(peak_vals(i,4)), 'linestyle','none', 'Marker', 'o', 'Color', 'r'); 
+        plot([xint_t_lesser, xint_t_greater], Ht_p*ones(1,2), 'pg', 'MarkerSize',10);
+        hold off;
+        
+        % visualize Hc_p
+        figure(2);
+        subplot(1,2,2);
+        title('crest');
+        hold on;
+        plot(xc, yc, 'Color', 'black');
+        plot(xc, ones(size(xc))*Hc_p, 'Color', 'm'); 
+        plot(time(peak_vals(i+1,2)), input_sig(peak_vals(i+1,2)), 'linestyle','none', 'Marker', 'o', 'Color', 'r'); 
+        plot([xint_c_lesser, xint_c_greater], Hc_p*ones(1,2), 'pg', 'MarkerSize',10);
+        hold off;
+        
+        
+        % morphological features
+        morph_matrix(f_idx, p_idx) = It_p / (Ht*p);
+        morph_matrix(f_idx, p_idx+50) = Ic_p / (Hc*p);
+
+        
+        % increment feature index
+        p_idx = p_idx + 1;
+
+    end
+    
+    % increment vector index
+    f_idx = f_idx + 1;
+    
+end
+
+end
+
--- generate_features.m
+++ generate_features.m
@@ -3,7 +3,9 @@ clc;
 close all;
 clear all;
 
+
 %% Import Data
+
 load('Data/data.mat')
 I1 = data2file(:,1);
 Q1 = data2file(:,2);
@@ -13,11 +15,12 @@ Q2 = data2file(:,4);
 % CB2 = data2file(:,6);
 time = data2file(:,7);
 
+
 %% Preprocess Data
-% start = find(time==201, 1);
-start = 1;
-stop = find(time==500, 1); % define the stopping time (default: 200seconds)
-% stop = length(time);
+
+start = find(time==0, 1);
+stop = find(time==500, 1); % define the stopping time (default: 500seconds)
+% stop = find(time==time(end),1); % go until end of trace
 
 % splice data based on start/stop points
 I1 = I1(start:stop);
@@ -38,11 +41,15 @@ time = time(start:stop);
 source1 = -source1;
 source2 = -source2;
 
+
 %% EMD
+
 [src1_filtered, k_src1, imf_src1, ~] = filter_by_emd(source1);
 [src2_filtered, k_src2, imf_src2, ~] = filter_by_emd(source2);
 
+
 %% Segmentation
+
 minpeakdist = 200;
 minpeakprom = 0.5;
 
@@ -78,239 +85,8 @@ hold off;
 
 %% Morphological Features
 
-figure(2);
-% pause;
-
-% initialize feature vectors
-morph_feat_1 = zeros(size(peaks1,1)-1, 100); % 100 feats/segment
-
-% trough
-Ht =  0; % height of the wave trough
-Ht_p = 0; % p% of height H_t from minimum point
-It_p = 0; % intercept of the waveform at height Ht_p
-
-% crest
-Hc =  0; % height of the wave crest
-Hc_p = 0; % p% of height H_t from maximum point
-Ic_p = 0; % intercept of the waveform at height Hc_p
-
-
-f_idx = 1;
-for i = 1:1:size(peaks1,1)-1 % for each segment (later consider overlappin segments)
-    
-    % Figure label
-    clf;
-    sgtitle(['Source 1: ', 'Segment ', num2str(f_idx), ' of ', num2str(size(morph_feat_1,1))]);
-    
-    % TROUGH
-    xt = time(peaks1(i,2):peaks1(i+1,2),1);
-    yt = src1_filtered(peaks1(i,2):peaks1(i+1,2),1);    
-    Ht = min(peaks1(i,1), peaks1(i+1,1)) - peaks1(i,3); %modified to accomodate for uneven values
-   
-    t_idx = 1;
-    for p = .02:.02:1
-        
-        % define height percentage (Ht_p)
-        Ht_p = Ht * p + peaks1(i,3);
-        if p == 1   % just to avoid sigfig issue
-            Ht_p = min(peaks1(i,1), peaks1(i+1,1)); 
-        end
-        
-        % find intersections
-        [x0, y0] = intersections(xt, yt, xt, Ht_p*ones(size(xt)));
-        x0_greater = min(x0(x0>time(peaks1(i,4))));
-        x0_lesser = max(x0(x0<time(peaks1(i,4))));
-        
-        % Compute width
-        It_p = x0_greater - x0_lesser;
-
-        % visualize Ht_p
-        figure(2);
-        subplot(1,2,1);
-        title('trough');
-        hold on;
-        plot(xt, yt, 'Color', 'black');
-        plot(xt, ones(size(xt))*Ht_p, 'Color', 'm'); 
-        plot(time(peaks1(i,4)), src1_filtered(peaks1(i,4)), 'linestyle','none', 'Marker', 'o', 'Color', 'r'); 
-        plot([x0_lesser, x0_greater], Ht_p*ones(1,2), 'pg', 'MarkerSize',10);
-        hold off;
-    
-        % morphological features
-        morph_feat_1(f_idx, t_idx) = It_p / (Ht*p);
-        
-        % increment feature index
-        t_idx = t_idx + 1;
-
-    end
-    
-   
-    % CREST
-    xc = time(peaks1(i,4):peaks1(i+1,4),1);
-    yc = src1_filtered(peaks1(i,4):peaks1(i+1,4),1);    
-    Hc = peaks1(i+1,1) - max(peaks1(i,3), peaks1(i+1,3)); %modified to accomodate for uneven values
-   
-    c_idx = 51;
-    for p = .02:.02:1
-        
-        % define crest percentage (Hc_p)
-        Hc_p = peaks1(i+1,1) - Hc * p;
-        if p == 1   % just to avoid sigfig issue
-            Hc_p = max(peaks1(i,3), peaks1(i+1,3)); 
-        end
-        
-        % find intersections
-        [x0, y0] = intersections(xc, yc, xc, Hc_p*ones(size(xc)));
-        x0_greater = min(x0(x0>time(peaks1(i+1,2))));
-        x0_lesser = max(x0(x0<time(peaks1(i+1,2))));
-        
-        % Compute width
-        Ic_p = x0_greater - x0_lesser;
-
-        % visualize Hc_p
-        figure(2);
-        subplot(1,2,2);
-        title('crest');
-        hold on;
-        plot(xc, yc, 'Color', 'black');
-        plot(xc, ones(size(xc))*Hc_p, 'Color', 'm'); 
-        plot(time(peaks1(i+1,2)), src1_filtered(peaks1(i+1,2)), 'linestyle','none', 'Marker', 'o', 'Color', 'r'); 
-        plot([x0_lesser, x0_greater], Hc_p*ones(1,2), 'pg', 'MarkerSize',10);
-        hold off;
-    
-
-        % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
-
-        % morphological features
-        morph_feat_1(f_idx, c_idx) = Ic_p / (Hc*p);
-        
-        % increment feature index
-        c_idx = c_idx + 1;
-
-    end
-    
-
-    % increment vector index
-    f_idx = f_idx + 1;
-    
-end
-
-
-% initialize feature vectors
-morph_feat_2 = zeros(size(peaks2,1)-1, 100); % 100 feats/segment
-
-% trough
-Ht =  0; % height of the wave trough
-Ht_p = 0; % p% of height H_t from minimum point
-It_p = 0; % intercept of the waveform at height Ht_p
-
-% crest
-Hc =  0; % height of the wave crest
-Hc_p = 0; % p% of height H_t from maximum point
-Ic_p = 0; % intercept of the waveform at height Hc_p
-
-
-f_idx = 1;
-for i = 1:1:size(peaks2,1)-1 % for each segment (later consider overlappin segments)
-    
-    % Figure label
-    clf;
-    sgtitle(['Source 2: ', 'Segment ', num2str(f_idx), ' of ', num2str(size(morph_feat_2,1))]);
-    
-    % TROUGH
-    xt = time(peaks2(i,2):peaks2(i+1,2),1);
-    yt = src2_filtered(peaks2(i,2):peaks2(i+1,2),1);    
-    Ht = min(peaks2(i,1), peaks2(i+1,1)) - peaks2(i,3); %modified to accomodate for uneven values
-   
-    t_idx = 1;
-    for p = .02:.02:1
-        
-        % define height percentage (Ht_p)
-        Ht_p = Ht * p + peaks2(i,3);
-        if p == 1   % just to avoid sigfig issue
-            Ht_p = min(peaks2(i,1), peaks2(i+1,1)); 
-        end
-        
-        % find intersections
-        [x0, y0] = intersections(xt, yt, xt, Ht_p*ones(size(xt)));
-        x0_greater = min(x0(x0>time(peaks2(i,4))));
-        x0_lesser = max(x0(x0<time(peaks2(i,4))));
-        
-        % Compute width
-        It_p = x0_greater - x0_lesser;
-
-        % visualize Ht_p
-        figure(2);
-        subplot(1,2,1);
-        title('trough');
-        hold on;
-        plot(xt, yt, 'Color', 'black');
-        plot(xt, ones(size(xt))*Ht_p, 'Color', 'm'); 
-        plot(time(peaks2(i,4)), src2_filtered(peaks2(i,4)), 'linestyle','none', 'Marker', 'o', 'Color', 'r'); 
-        plot([x0_lesser, x0_greater], Ht_p*ones(1,2), 'pg', 'MarkerSize',10);
-        hold off;
-    
-
-        % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
-
-        % morphological features
-        morph_feat_2(f_idx, t_idx) = It_p / (Ht*p);
-        
-        % increment feature index
-        t_idx = t_idx + 1;
-
-    end
-    
-   
-    % CREST
-    xc = time(peaks2(i,4):peaks2(i+1,4),1);
-    yc = src2_filtered(peaks2(i,4):peaks2(i+1,4),1);    
-    Hc = peaks2(i+1,1) - max(peaks2(i,3), peaks2(i+1,3)); %modified to accomodate for uneven values
-   
-    c_idx = 51;
-    for p = .02:.02:1
-        
-        % define crest percentage (Hc_p)
-        Hc_p = peaks2(i+1,1) - Hc * p;
-        if p == 1   % just to avoid sigfig issue
-            Hc_p = max(peaks2(i,3), peaks2(i+1,3)); 
-        end
-        
-        % find intersections
-        [x0, y0] = intersections(xc, yc, xc, Hc_p*ones(size(xc)));
-        x0_greater = min(x0(x0>time(peaks2(i+1,2))));
-        x0_lesser = max(x0(x0<time(peaks2(i+1,2))));
-        
-        % Compute width
-        Ic_p = x0_greater - x0_lesser;
-
-        % visualize Hc_p
-        figure(2);
-        subplot(1,2,2);
-        title('crest');
-        hold on;
-        plot(xc, yc, 'Color', 'black');
-        plot(xc, ones(size(xc))*Hc_p, 'Color', 'm'); 
-        plot(time(peaks2(i+1,2)), src2_filtered(peaks2(i+1,2)), 'linestyle','none', 'Marker', 'o', 'Color', 'r'); 
-        plot([x0_lesser, x0_greater], Hc_p*ones(1,2), 'pg', 'MarkerSize',10);
-        hold off;
-    
-
-        % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
-
-        % morphological features
-        morph_feat_2(f_idx, c_idx) = Ic_p / (Hc*p);
-        
-        % increment feature index
-        c_idx = c_idx + 1;
-
-    end
-    
-
-    % increment vector index
-    f_idx = f_idx + 1;
-    
-end
-
+morph_feat_1 = morph_features(time, src1_filtered, peaks1);
+morph_feat_2 = morph_features(time, src2_filtered, peaks2);
 
 
 %% Fuzzy Wavelet Packet Transform Algorithm (FWPT) Features
